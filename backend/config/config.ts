1571d13fce530342a0d1d9a0e9692f0a4826772e
Virendra Jopale <virujopale@gmail.com>
2024-02-11 18:21:43 +0530

    c5

--- backend/config/config.ts
+++ backend/config/config.ts
@@ -8,7 +8,7 @@ dotenv.config()
 
 const MONGO_USER=process.env.MONGO_USER || '';
 const MONGO_PASS=process.env.MONGO_PASS || '';
-const MONGO_URL=`mongodb+srv://virujopale99:Viru%40123.45@blog-web.nr5x3jq.mongodb.net/Todo`
+const MONGO_URL=process.env.DbURL || '';
 const PORT=process.env.PORT?Number(process.env.PORT):3000;
 const HOSTNAME=process.env.HOSTNAME || "localhost"
 
